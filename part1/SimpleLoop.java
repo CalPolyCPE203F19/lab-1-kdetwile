2f6cfe0648ba28689ef3eb785372c0578cc36386
kdetwile <49246703+kdetwile@users.noreply.github.com>
2019-09-27 16:30:53 -0700

    Add files via upload

--- part1/SimpleLoop.java
+++ part1/SimpleLoop.java
@@ -7,9 +7,12 @@ class SimpleLoop
          done without a loop, but the point is to
          practice the syntax for a loop.
       */
+        if (low == high) {
+        	return (low * 2);
+        }
         int count = low;
         int increase_num = low + 1;
-        while (increase_num < (high)) {
+        while (increase_num < high) {
         	count = (count + increase_num);
         	increase_num++;
         }
